5b820525a94a7493161368a390100f59886c5b52
mattmcphee <matt.imcphee@gmail.com>
2024-04-24 15:30:13 +1000

    fixed set-allnewitmestosentcollection function
    now correctly sets the collections of items in new and sent to just sent

--- 1.0.0.0/Public/Set-AllNewItemsToSentCollection.ps1
+++ 1.0.0.0/Public/Set-AllNewItemsToSentCollection.ps1
@@ -12,18 +12,23 @@ Set-AllNewItemsToSentCollection
 function Set-AllNewItemsToSentCollection {
     New-BWLoginUnlock
 
-    $sentCollId = '0f3a5a11-66dc-4cf2-af8a-b00a0014d120'
-    $newCollId = 'd663b595-1c92-43cc-82d5-b00a0014c32e'
+    $sentCollId = "0f3a5a11-66dc-4cf2-af8a-b00a0014d120"
+    $newCollId = "d663b595-1c92-43cc-82d5-b00a0014c32e"
+    $orgId = "f97245e0-5ce6-4c36-ac40-ad0c004ae861"
 
     Write-Host 'getting list of items in users/new...'
-    $newItems = Invoke-Command { bw list items --collectionid $newCollId } | ConvertFrom-Json
+    $newItems = (bw list items --collectionid $newCollId) | ConvertFrom-Json
     
     Write-Host 'items retrieved.'
-
     Write-Host "setting each item's collection to users/sent ONLY"
+
     foreach ($item in $newItems) {
-        Invoke-Command -ScriptBlock { "[$sentCollId]" | bw encode | bw edit item-collections $item.id } | Out-Null
-        Write-Host "'$($item.name)' done."
+        $itemId = $item.id
+        $itemName = $item.name
+        $sentCollIdString = '["' + $sentCollId + '"]'
+        $sentCollIdString | bw encode | bw edit item-collections $itemId --organizationid $orgId
+        Write-Host "'$($itemName)' done."
     }
+
     Write-Host 'all items in users/new removed.'
 }
